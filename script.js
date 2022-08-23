230ebb283cc1b2c05a895620d37ead5f5b793edb
Ioannis ZISAKIS <47507065+gianniszisakis@users.noreply.github.com>
2022-08-23 21:10:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,43 +4,40 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1; //the number that we shou
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function(message){
+    document.querySelector(".guessText").textContent = message;
+}
+
+
 document.querySelector(".check").addEventListener("click", function(){ //when the user click the check button
     const guess = Number(document.querySelector(".guess").value); //the value of the guess box
 
     //if the user does not provide a number
     if(!guess){
-        document.querySelector(".guessText").textContent = "Please fill a number (1-20)"
+        displayMessage("Please fill a number (1-20)"); 
     }
     else if(guess === secretNumber){ //if the user guess the right number
-        document.querySelector(".guessText").textContent = "You found it!"; //message apears
+        displayMessage("You found it!"); //message apears
         document.querySelector(".guessText").style.color = "red";
         document.querySelector(".centerDIV").style.backgroundColor = "green"; //change background color of DIV
-        highscore = score; //equals the score with highscore
-        document.querySelector(".highscore").textContent = highscore; //assign the highscore to the span
+        
+        //if score is higher than highscore then new highscore is assigned
+        if(score > highscore){ 
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore; 
+        }
     }
-    else if(guess > secretNumber){ //if number is greater than the correct answer
+    else if(guess !== secretNumber){ //if the answer is wrong
         if(score > 1){
-            document.querySelector(".guessText").textContent = "Too High!"; //message appears
+            displayMessage(guess > secretNumber ? "Too High!" :  "Too Low!");
             score--; //decrease the score by 1
-            document.querySelector(".score").textContent = score; //assign the score to the result
+            document.querySelector(".score").textContent = score;
         }
         else{
-            document.querySelector(".guessText").textContent = "You lost";
+            displayMessage("You lost");
             document.querySelector(".centerDIV").style.backgroundColor = "red"; //change background color of DIV
             document.querySelector(".score").textContent = 0; //restore the value of the score to 0
         }
-    }
-    else if(guess < secretNumber){
-        if(score > 1){
-            document.querySelector(".guessText").textContent = "Too Low!"; //message appears
-            score--; //decrease the score by 1
-            document.querySelector(".score").textContent = score; //assign the score to the result
-        }
-        else{
-            document.querySelector(".guessText").textContent = "You lost";
-            document.querySelector(".centerDIV").style.backgroundColor = "red"; //change background color of DIV
-            document.querySelector(".score").textContent = 0; //restore the value of the score to 0
-        }       
     }
 
     //reset button behavior
